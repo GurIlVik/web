0aa06d9897049851c2950d97b5708013d14c68c4
GurIlVik <gilvik1@yandex.ru>
2023-02-02 10:57:24 +0300

    сщздана ветвь git branch feature

--- collector/account/views.py
+++ collector/account/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 def user_login(request):
     if request.method == 'POST':
@@ -25,3 +25,21 @@ def user_login(request):
         request,
         'account/login.html',
         {'form': form})
+    
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(
+            request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(
+                commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(
+        request,
+        'account/register.html',
+        {'form': user_form})
