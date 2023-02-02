f9fcd0d9be31f98841895c22885bd13f482941c0
GurIlVik <gilvik1@yandex.ru>
2023-02-02 14:42:23 +0300

    работает страница, не работает статика

--- collector/collector/urls.py
+++ collector/collector/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
