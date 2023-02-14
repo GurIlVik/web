69bdc6ed6684feed6109fc9df43d57b907414f02
GurIlVik <gilvik1@yandex.ru>
2023-02-14 18:00:29 +0300

    Впмсал первую модель в модели main

--- collector/main/models.py
+++ collector/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Amalker(models.Model):
+    text = models.TextField(null=True, blank=True)
+    kateg = models.CharField(max_length=250) # указывается предмет или список ...ов коллекции
+    time = models.DateTimeField(auto_now_add=True, db_index=True)
+    info = models.CharField(max_length=250)
+    
