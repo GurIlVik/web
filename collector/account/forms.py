951bf17070c11b2dc40d157d7ad69db7c3d7c840
GurIlVik <gilvik1@yandex.ru>
2023-02-17 17:04:51 +0300

    добавил почту в обязательный параметр

--- collector/account/forms.py
+++ collector/account/forms.py
@@ -1,33 +1,93 @@
 from django.contrib.auth.models import User
 from django import forms
 
+from django import forms 
+from django.contrib.auth.models import User 
+from django.contrib.auth.forms import UserCreationForm 
+from django.core.exceptions import ValidationError 
+from django.forms.fields import EmailField 
+from django.forms.forms import Form 
+# Источник: https://pythonpip.ru/django/django-usercreationform-sozdanie-novogo-polzovatelya
 
-class LoginForm(forms.Form):
+class LoginForm(Form):
+    print('lwerjngelrjkgn')
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
    
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label='Repeat password',
-        widget=forms.PasswordInput)
+# class UserRegistrationForm(forms.ModelForm):
+class UserRegistrationForm(UserCreationForm):  
+    print('lwerjngrfrjkgn')  
+    username = forms.CharField(label='Пользователь', min_length=3, max_length=150) 
+    email = EmailField(label='email') 
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput) 
+    password2 = forms.CharField(label='Повторить пароль', widget=forms.PasswordInput) 
+ 
+    def username_clean(self): 
+        print('lwerjngrfrjkgn1')  
+        username = self.cleaned_data['username'].lower() 
+        new = User.objects.filter(username = username) 
+        if new.count(): 
+            raise ValidationError("User Already Exist") 
+        return username 
+ 
+    def email_clean(self): 
+        print('lwerjngrfrjkgn2')  
+        email = self.cleaned_data['email'].lower() 
+        new = User.objects.filter(email=email) 
+        if new.count(): 
+            raise ValidationError(" Email Already Exist") 
+        return email 
+ 
+    def clean_password2(self): 
+        print('lwerjngrfrjkgn3')  
+        password1 = self.cleaned_data['password1'] 
+        password2 = self.cleaned_data['password2'] 
+ 
+        if password1 and password2 and password1 != password2: 
+            raise ValidationError("Password don't match") 
+        return password2 
+ 
+    def save(self, commit = True): 
+        print('lwerjngrfrjkgn4') 
+        user = User.objects.create_user( 
+            self.cleaned_data['username'], 
+            self.cleaned_data['email'], 
+            self.cleaned_data['password1'] 
+        ) 
+        print('lwerjngrfrjkgn5') 
+        return user 
+# Источник: https://pythonpip.ru/django/django-usercreationform-sozdanie-novogo-polzovatelya
+    
+    
+    
+    
+    
+    
+    
+    # useremail = forms.EmailField(
+    #     label = "Введите email"
+    # )
+    # password = forms.CharField(
+    #     label='Пароль',
+    #     widget=forms.PasswordInput)
+    # password2 = forms.CharField(
+    #     label='Повтор Пароля',
+    #     widget=forms.PasswordInput)
 
-    class Meta:
-        model = User
-        fields = (
-            'username',
+    # class Meta:
+    #     model = User
+    #     fields = (
+    #         'username',)
             # 'first_name',
-            'email',)
+            # 'email',)
 
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError(
-                'Passwords don\'t match.')
-        return cd['password2'] 
+    # def clean_password2(self):
+    #     cd = self.cleaned_data
+    #     if cd['password'] != cd['password2']:
+    #         raise forms.ValidationError(
+    #             'Passwords don\'t match.')
+    #     return cd['password2'] 
 
 
 
--- collector/account/views.py
+++ collector/account/views.py
@@ -4,9 +4,15 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
-
+from django.contrib.auth import forms 
+from django.shortcuts import redirect, render 
+from django.contrib import messages 
+from django.contrib.auth.forms import UserCreationForm 
+# from .forms import CustomUserCreationForm 
+# Источник: https://pythonpip.ru/django/django-usercreationform-sozdanie-novogo-polzovatelya
 
 def user_login(request):
+    print('wergfqfgqerngrfrjkgn')  
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -30,25 +36,43 @@ def user_login(request):
         {'form': form})
     
 def register(request):
+    form = UserRegistrationForm()
     if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
-            new_user.save()
-            return render(request, 'account/register_done.html', {'new_user': new_user})
-    else:
-        user_form = UserRegistrationForm()
-    return render(
-        request,
-        'account/register.html',
-        {'form': user_form},
-        # {'user_form': user_form},
-        )
+        form = UserRegistrationForm(request.POST) 
+        print('lwerjngrfrjkgn34')  
+        if form.is_valid(): 
+            print('lwerjngrfrjkgn45')  
+            form.save() 
+    else: 
+        form = UserRegistrationForm() 
+    context = { 
+        'form':form 
+    } 
+    return render(request, 'account/register.html', context) 
+# Источник: https://pythonpip.ru/django/django-usercreationform-sozdanie-novogo-polzovatelya
+    #     user_form = UserRegistrationForm(request.POST)
+    #     if user_form.is_valid():
+    #         new_user = user_form.save(commit=False)
+    #         new_user.set_password(user_form.cleaned_data['password'])
+    #         new_user.save()
+    #         return render(request, 'account/register_done.html', {'new_user': new_user})
+    # else:
+    #     user_form = UserRegistrationForm()
+    # return render(
+    #     request,
+    #     'account/register.html',
+    #     {'form': user_form},
+    #     # {'user_form': user_form},
+    #     )
+    
+    
+    
 
 @login_required
 def personal_page (request, user_id=1):
+    print('lwerjngewgwertgtrwegtgn')  
     return HttpResponse('<h1>%s</h1>' % user_id)
 
 def login_email (request):
+    print('lewgwergtwgrtwgertewfgwregfrjkgn')  
     return HttpResponse('<h1>В стадии разработки</h1>')
