b4f162debec2965907a5d987728049d272763fec
GurIlVik <gilvik1@yandex.ru>
2023-03-10 15:07:15 +0300

    исправил -  пути в сетинге

--- collector/collector/settings.py
+++ collector/collector/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account', 
-    'start',
-    'main',
+    'account.apps.AccountConfig', 
+    'start.apps.StartConfig',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
