e78ff0b3879c48c74b974b8adcd638cf933478e5
GurIlVik <gilvik1@yandex.ru>
2023-03-13 12:11:54 +0300

    добавлены имена путей

--- collector/main/urls.py
+++ collector/main/urls.py
@@ -4,6 +4,6 @@ from .views import  main_page
 
 
 urlpatterns = [
-    path('', main_page),
+    path('', main_page, name='main',)
     # path('', main_page, name='index'),
 ]
--- collector/start/urls.py
+++ collector/start/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.privet, name='index'), 
+    path('', views.privet, name='start'), 
 ]
 
 
