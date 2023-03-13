c1421e1b096c6e4e860b2990e0730eabd7fb2cfc
GurIlVik <gilvik1@yandex.ru>
2023-03-13 14:08:15 +0300

    вроде всплыло окно

--- collector/static/main/script.js
+++ collector/static/main/script.js
@@ -7,7 +7,12 @@ function find_cat(my_obj) {
     for (let item of catalog.children) {
         if (item.innerHTML.includes(letter)) {
             item.style.display = 'inline';
-
         } else { item.style.display = 'None';
          }
     }}
+
+
+function menuPoint2() {
+    let blok2 = document.getElementById('alternative_global_2').style.zIndex = '10';
+
+}
