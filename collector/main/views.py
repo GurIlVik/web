bae7f569b6de292c50d7ba0888c046f73284b793
GurIlVik <gilvik1@yandex.ru>
2023-03-23 15:17:56 +0300

    причесал вьюс

--- collector/main/views.py
+++ collector/main/views.py
@@ -3,26 +3,37 @@ from django.http import HttpResponse
 from .models import Category
 from .forms import ProstoList
 
-# Create your views here.
+
 def main_page(request):
     a = Category.objects.all().order_by('name')  # сортировка списка по имени в базе
     b = ProstoList('prosto list')
-    if request.method == 'POST':
-        form = ProstoList(request.POST) 
-        print('ejfnvfejknefvev')
-        print(form)
-        
-        print(request.POST) 
-        if form.is_valid(): 
-            #form = form.cleaned_data
-            print(form)#.cleaned_data)
-            #for i in form:
-            #    return HttpResponse('i')
+    predmet_collection_list = []   # В этом списке выбранные категории предметов
     context = {
         'a': a,
         "form2" : b
     }
-    return render(
-        request,
-        'main/index.html', context
-        )
+    if request.method == 'POST':
+        form = ProstoList(request.POST) 
+        if form.is_valid(): 
+            form = form.cleaned_data
+            predmet_collection_list = motod_main_page_1(form['pole'])
+            print(predmet_collection_list)
+        return render(request, 'main/index.html', context)
+    
+    return render(request, 'main/index.html', context)
+    
+    
+# Вспомогательная функция сортировки коллекционных предметов пришедших с формы после JScript
+def motod_main_page_1(a):
+    list_a = []
+    str_a = ''
+    for i in range(len(a)):
+        if a[i] == ',':
+            list_a.append(str_a)
+            str_a = ''
+        elif a[i] == " " and a[i-1] == ',':
+                pass
+        else:
+            str_a += a[i]
+    return list_a
+
