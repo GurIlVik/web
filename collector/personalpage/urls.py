cf99145541e5756cb3fcae7d44311c5991f4c06c
GurIlVik <gilvik1@yandex.ru>
2023-03-29 17:48:41 +0300

    не удалось настроить блок страницы пользователя.

--- collector/personalpage/urls.py
+++ collector/personalpage/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('<user>', views.personal_page, name='personal_page'),
+    path(f'<user>', views.personal_page, name='personal_page'),
 ]
--- collector/personalpage/views.py
+++ collector/personalpage/views.py
@@ -2,12 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 # from django.contrib.auth import authenticate, login, logout
 # from .forms import LoginForm, UserRegistrationForm, LoginForm_Email, LoginFormToken
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
+from . import urls
 # from .models import UserTemporaryModels, UserTemporaryToken
 
-
+@ login_required
 def personal_page(request, user): 
-    context = {'form1': user}
-    return render(request, 'personalpage/index.html', context)
+    print(user)
+    
+    a = False
+    context = {'form' : a}
+
+    for us in User.objects.all():
+        if user == us.username:
+            print(us.username)   
+            a= True
+            
+            return render(request, 'personalpage/index.html', context)
+        
+    return HttpResponse('такого пользователя нет')
