b753eb6e581f0f0ff8031e5d22e7d23d96bfb83b
GurIlVik <gilvik1@yandex.ru>
2023-03-30 12:18:36 +0300

    переделал логику во вью

--- collector/personalpage/views.py
+++ collector/personalpage/views.py
@@ -11,13 +11,16 @@ from . import urls
 def personal_page(request, user): 
     nik_reguest = getting_nickname_request(request)  # получение имени пришедшего пользователя
     search_user = user_page_search(user)  # получение булевого есть ли такой пользователь (тру/фальш)
+    registered_user = chek_user_register(request)  # логик тру пользователь зарегистрирован или нет фальш
     print(user)             # имя страницы пользователя
     print(nik_reguest)      # имя зашедшего на страницу
     print(search_user)      # тру-страница есть фальш - страницы нет
+    print(registered_user)  # тру-зарегистрирован | нет фальш
     if search_user:
         password = chek_user_access(user, nik_reguest) # логик - тру страница индивида или нет фальш
         context = {'nik_name' : nik_reguest,
-                   'access' : password}
+                   'access' : password,
+                   'register' : registered_user}
         return render(request, 'personalpage/index.html', context)
         
     return HttpResponse('такого пользователя нет')
@@ -31,16 +34,20 @@ def getting_nickname_request(request):
 
 # функция проверки наличия страницы пользователя
 def user_page_search(user):
-    search_user = False
     for us in User.objects.all():
         if user == us.username:
-            search_user = True
-    return search_user
+            return True
+    return False
 
 # функция получения логики (страница ли пользователя)
 def chek_user_access(user, nik_reguest):
-    password = False
     if user == nik_reguest:
-        password = True
-    return password
+        return True
+    return False
 
+# логика зарегистрированость пользователя
+def chek_user_register(request):
+    if request.user.is_authenticated:
+        return True
+    return False
+    
