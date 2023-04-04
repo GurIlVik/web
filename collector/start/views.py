7432971e482cbfe0559485dac59191a6272e902d
GurIlVik <gilvik1@yandex.ru>
2023-04-04 13:16:10 +0300

    исправил путь с главной на личную

--- collector/start/views.py
+++ collector/start/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from personalpage.views import getting_nickname_request
+# from django.contrib.auth.models import User
 
 
 def privet(request):
+    req = request.user
+    print(req)
+    context = {
+        'puth_paesonalpage' : req, 
+    }
     return render(
         request,
-        'start/index.html',
+        'start/index.html', 
+        context
         )
 
 
