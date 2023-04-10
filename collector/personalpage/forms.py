9de0e3a183ef6724f27e79584d771a193ca114aa
GurIlVik <gilvik1@yandex.ru>
2023-04-10 17:11:41 +0300

    первичная форма ввода статьи изером

--- collector/personalpage/forms.py
+++ collector/personalpage/forms.py
@@ -1,23 +1,27 @@
 from django import forms 
 from django.forms import ClearableFileInput
-from main.models import Category
+from main.models import Category, Allowance
 # from django.forms.forms import Form 
 
 
 class NewArticleForm(forms.Form):
-    title = forms.CharField(initial="название", max_length=150, 
+    title = forms.CharField(initial="название", max_length=150,                            # название статьи
             widget=forms.TextInput(attrs={'class': 'main_form_title'})) 
-    categories = forms.ModelChoiceField( 
+    categories = forms.ModelChoiceField(                                                   # предмет коллекционирования
             queryset=Category.objects.all().order_by('name'),
             label='Предмет коллекционирования',
             empty_label = 'К выбору обязателен',
             # widget=forms.TextInput(attrs={'class': 'main_form_categories'}),
             )  
-    topic = forms.CharField(initial="статья/продажа/покупка",  max_length=150,
+    topic = forms.CharField(initial="статья/продажа/покупка",  max_length=150,             # категория сообщения 
             widget=forms.TextInput(attrs={'class': 'main_form_topic'}))
-    text = forms.CharField( widget=forms.Textarea, initial="текст", 
+    text = forms.CharField( widget=forms.Textarea, initial="текст",                        # текст сообщения
             ) 
-    photo = forms.FileField(label='введите файл', 
+    allowance = forms.CharField(initial="допуск", max_length=50,                            # допуск к статьи
+        #     queryset=Allowance.objects.all().order_by('name'),
+            )
+    
+    photo = forms.FileField(label='введите файл',                                          # загрузка файлов
             initial="сюда:", 
             widget = ClearableFileInput(attrs={'multiple': True, 'class': 'main_form_text'}), 
             )
