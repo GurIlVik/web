843d79919be402eefd932ebe2c8e392fc17d7269
GurIlVik <gilvik1@yandex.ru>
2023-04-11 11:52:02 +0300

    добавлена всплывалка на персональной

--- collector/static/personalpage/script.js
+++ collector/static/personalpage/script.js
@@ -3,18 +3,18 @@
 
 function article() {
     console.log('kjwdhbv');
-    let global = document.getElementById('global');
-    let div_main = document.createElement('div');
-    console.log(global);
-    div_main.style.display = 'absolute';
-    div_main.style.top = '7%'
-    div_main.style.left = '7%'
-    div_main.position = 'flex';
-    div_main.style.height = '600px';
-    div_main.style.width = '600px';
-    div_main.style.zIndex = '1';
-    div_main.style.background = 'c0c4ac79';
-    global.appendChild(div_main);
+    let global = document.getElementById('alternativ_global');
+    // let div_main = document.createElement('div');
+    // console.log(global);
+    global.style.display = 'block';
+    // div_main.style.top = '7%'
+    // div_main.style.left = '7%'
+    // div_main.position = 'flex';
+    // div_main.style.height = '600px';
+    // div_main.style.width = '600px';
+    // div_main.style.zIndex = '1';
+    // div_main.style.background = 'c0c4ac79';
+    // global.appendChild(div_main);
     // div_main.value = 'ewfpijnvowirnv';
     // global.innerHTML = `<p><span>&ensp;&ensp;</span><span class="Child_main_menu2">irbnwlrbvlwrbv</span><span>,</span></p> `
 
