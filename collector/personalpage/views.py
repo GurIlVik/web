ab923d0dc4e9e48933d913bf9cf751d7894f674b
GurIlVik <gilvik1@yandex.ru>
2023-04-19 16:28:07 +0300

    логик допуска на личную страницу

--- collector/personalpage/views.py
+++ collector/personalpage/views.py
@@ -27,33 +27,41 @@ def personal_page(request, user):
     d = NewArticle.objects.filter(author=user)
     e = PersonalInformationUser()
     f = Catalogy.objects.all().order_by('name')
-    g = ''                                      # логика получения информации о заполненности анкеты потльзователя
+    g = False                                     # логика получения информации о заполненности анкеты потльзователя форма1
     h = None                                    # отображение информации о пользователи из модели
     k = []                                       #   отображение коллекционного листа
     l = AllowanceForm()
     m = SpecialInfoUser()
+    n = False        # есть ли анкета допусков форма2
+    o = 3            # допуск к личной странице
+    p = 3            # допуск к личной инормации  3-нет 2интересанты 1-клуб 0-все
+    q = 3            # допуск к мессенжеру  3-нет 2интересанты 1-клуб 
+    acesses = Allowance.objects.filter(user__username = user)
+    if acesses:
+        n, o, p, q = function_acess_user(user)
     title = 'Кабинет'
     menu = ['К СЕБЕ', 'НА ГЛАВНУЮ', 'РЕГИСТРАЦИЯ',]
     menu1 = ['НАСТРОЙКИ', 'НАПИСАТЬ', 'К ОБЩЕСТВУ', 'ВЫХОД',]
     menu2 = ['К СЕБЕ', 'К ОБЩЕСТВУ', 'ВЫХОД',]
-    
-    if password:                                           # если это страница юзера
-        menu = menu1
-        g = PresentationUser.objects.filter(user__username = nik_reguest)
-        for i in g:
+    list_goest = False    # интересант ли гость // лож - нет
+    list_collection = PresentationUser.objects.filter(user__username = user)
+    if list_collection:
+        for i in list_collection:
             h = i
             k = func_str_for_list(i.interest)
-            print(k)
             if i.in_publishid == True:
                 g = True
-            else:
-                g = False
+    if password:                                           # если это страница юзера
+        menu = menu1
     elif registered_user:
         menu = menu2
-    print(d)
-    for i in d:
-        print(i.author)
-    print(k)
+        us_g = PresentationUser.objects.filter(user__username = nik_reguest)
+        for i in us_g:
+            for j in func_str_for_list(i.interest):
+                if j in k:
+                    list_goest = True
+
+  
     context = {'nik_name' : nik_reguest,
                    'access' : password,
                    'register' : registered_user,
@@ -65,14 +73,21 @@ def personal_page(request, user):
                    'menu' : menu,
                    'form_info' : e,
                    'list_a': f,
-                   'logik_1': g,
+                   'logik_1': g,     # отображение в случае если 1ая форма заполнена(1) не заполнена(0)
                    'persona' : h,
                    'predmets' : k,
                    'acess_form' : l,
                    'secret_form' : m,
+                   'acess_key' : n,
+                   'acess_page' : o,
+                   'acess_info' : p,
+                   'acess_mass' : q,
                    }
+    print(list_goest)
     if search_user == False:
         return HttpResponse('такого пользователя нет')
+    if password == False and o == 3 or registered_user == False and o == 1 or list_goest == False and o == 2:
+        return HttpResponse('пользователь закрыл свою страницу')
     else:
         if request.method == 'POST':
             form = NewArticleForm(request.POST)
@@ -92,7 +107,6 @@ def personal_page(request, user):
                     с = Information_block.objects.create(
                         picture_author = 'фото',
                         page_author = b,       # ник автора с отправкой на его страницу
-                        # time_publication = models.DateTimeField(auto_now_add=True)
                         categories = c['categories'],        # категории по списку предметов коллекционирования
                         topic_interest = c['topic'],      # категории по списку интереса и поиска
                         table_contents = c['title'],
@@ -120,9 +134,6 @@ def personal_page(request, user):
                 return render(request, 'personalpage/index.html', context)
             elif form_user2.is_valid():
                 form_user2 = form_user2.cleaned_data
-                print(form_user2['for_page'])
-                print(form_user2['for_inform'])
-                print(form_user2['for_messeng'])
                 l = Allowance(
                     user = request.user,
                     for_page = function_acess(form_user2['for_page']),
@@ -131,7 +142,6 @@ def personal_page(request, user):
                     in_publishid = True,
                 )
                 l.save()
-                print(l.for_page)
                 return render(request, 'personalpage/index.html', context)
             elif form_user3.is_valid():
                 form_user3 = form_user3.cleaned_data
@@ -144,7 +154,6 @@ def personal_page(request, user):
                     in_publishid = True,
                 )
                 m.save()
-                print(m.name)
                 return render(request, 'personalpage/index.html', context)
             else:
                 print('что то идет не так')
@@ -179,7 +188,8 @@ def chek_user_register(request):
     if request.user.is_authenticated:
         return True
     return False
-    
+ 
+# функция преобразования строки из бызы данных в список   
 def func_str_for_list(inlist):
     res = []
     string = ''
@@ -192,13 +202,24 @@ def func_str_for_list(inlist):
                 string = ''
     return res
             
-            
+# функция получения цифрового значения доступов            
 def function_acess(info):
-    res = 0
-    print(len(str(info)))
+    res = 3
+    if 10 < len(str(info)) < 19:
+        res = 0
     if 20 < len(str(info)) < 33:
         res = 1
     if len(str(info)) > 33:
         res = 2
-    print(res)
     return res
+
+# функция получения доступа от хозяина страницы
+def function_acess_user(user):
+    acesses = Allowance.objects.filter(user__username = user)
+    if acesses:
+        acesses = Allowance.objects.get(user__username = user)
+        n = acesses.in_publishid
+        o = acesses.for_page
+        p = acesses.for_inform
+        q = acesses.for_messeng
+    return n, o, p, q
