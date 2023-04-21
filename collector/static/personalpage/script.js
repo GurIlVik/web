dd095654caffdba216277019c51922ddb7e282c0
GurIlVik <gilvik1@yandex.ru>
2023-04-21 17:27:17 +0300

    исп косяк в JSc нПС

--- collector/static/personalpage/script.js
+++ collector/static/personalpage/script.js
@@ -4,21 +4,7 @@
 function article() {
     console.log('kjwdhbv');
     let global = document.getElementById('alternativ_global');
-    // let div_main = document.createElement('div');
-    // console.log(global);
     global.style.display = 'block';
-    // div_main.style.top = '7%'
-    // div_main.style.left = '7%'
-    // div_main.position = 'flex';
-    // div_main.style.height = '600px';
-    // div_main.style.width = '600px';
-    // div_main.style.zIndex = '1';
-    // div_main.style.background = 'c0c4ac79';
-    // global.appendChild(div_main);
-    // div_main.value = 'ewfpijnvowirnv';
-    // global.innerHTML = `<p><span>&ensp;&ensp;</span><span class="Child_main_menu2">irbnwlrbvlwrbv</span><span>,</span></p> `
-
-
 }
 
 
@@ -84,8 +70,9 @@ function menuPoint25() {
     let value_list = document.getElementById('plusPredmet');
     value_list.innerHTML = `<p>Предмет коллекционирования:</p>`
     console.log(value_list.innerText)
-    let div_object2 = document.getElementById('q1');
+    let div_object2 = document.getElementById('id_interest');
     div_object2.value = ''
+    console.log(div_object2)
     div_object2.style.zIndex = '-10'
     console.log(div_object2.value);
 }
