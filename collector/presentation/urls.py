9229b2d3be3a350fcf811a6fbd3347bf90a62a41
GurIlVik <gilvik1@yandex.ru>
2023-05-12 16:24:00 +0300

    заполнение презентации

--- collector/presentation/urls.py
+++ collector/presentation/urls.py
@@ -5,6 +5,11 @@ from .views import *
 
 urlpatterns = [
     path('a', index_1),
+    path('b', index_2),
+    path('с', index_3),
+    path('d', index_4),
+    path('e', index_5),
+    
     # path('<author>/<id>', publication, name='author_publication'),
 
 ]
--- collector/presentation/views.py
+++ collector/presentation/views.py
@@ -7,3 +7,19 @@ from personalpage.models import PresentationUser
 def index_1(request):
     context = {'word1' : 'Клюб коллекционеров'}
     return render(request, 'presentation/index_1.html', context)
+
+def index_2(request):
+    context = {'word1' : 'Клюб коллекционеров'}
+    return render(request, 'presentation/index_2.html', context)
+
+def index_3(request):
+    context = {'word1' : 'Клюб коллекционеров'}
+    return render(request, 'presentation/index_3.html', context)
+
+def index_4(request):
+    context = {'word1' : 'Клюб коллекционеров'}
+    return render(request, 'presentation/index_4.html', context)
+
+def index_5(request):
+    context = {'word1' : 'Клюб коллекционеров'}
+    return render(request, 'presentation/index_5.html', context)
